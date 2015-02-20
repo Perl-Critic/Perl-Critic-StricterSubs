1828fe97dd44bbbe905f53bd662bcdc2df855344
Jeffrey Ryan Thalhammer <jeff@thaljef.org>
2015-02-20 10:47:37 -0800

    Bump VERSION to 0.05

--- lib/Perl/Critic/Policy/Modules/RequireExplicitInclusion.pm
+++ lib/Perl/Critic/Policy/Modules/RequireExplicitInclusion.pm
@@ -22,7 +22,7 @@ use Perl::Critic::StricterSubs::Utils qw(
 
 #-----------------------------------------------------------------------------
 
-our $VERSION = 0.04;
+our $VERSION = 0.05;
 
 my $expl =
     'Without importing a package, it is unlikely that references to things inside it even exist.';
